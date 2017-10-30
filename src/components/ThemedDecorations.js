c0e9b264595c0c69da2e28f7f4845e7cbc27c6ba
Moshe Levi <mo6709@gmail.com>
2017-10-30 18:02:28 -0400

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,24 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component {
+
+    render(){
+        //get the children and map them with a calback function that singn in to them a className with the them.
+        const chikldrnWithExtrClassNameProp = React.Children.map(this.props.children,
+            (child) => {
+                return React.cloneElement(child, {
+                    //1)how to assign a className to each child?
+                    //2)how to get the prop theme from the componnet  
+                    className: this.props.theme
+                });
+            } 
+        );
+        return(
+            <div className="decoration">
+                {chikldrnWithExtrClassNameProp}
+            </div>
+        );
+    }
+}
+
+export default ThemedDecorations; 
